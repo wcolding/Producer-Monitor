0a1339d03999d8a662c8435e1345b1f85e6e5ffe
Will Colding <william.colding@gmail.com>
2023-03-21 16:08:12 -0400

    Remove unused label lua scripts

--- Lua/Expanding_Mix_Label.lua
+++
@@ -1,8 +0,0 @@
-local button = self.parent.children[1]
-
-function init()
-  self.frame.x = button.frame.x
-  self.frame.y = button.frame.y
-  self.frame.w = button.frame.w
-  self.frame.h = button.frame.h
-end
